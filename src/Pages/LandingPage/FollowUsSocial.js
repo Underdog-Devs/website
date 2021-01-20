70e03ab4eb1ad51b508a7d83cea781db0a64014a
robertjallen <bobbidigi34@gmail.com>
2021-01-20 16:44:04 -0500

    add buttons leading to social media contacts

--- src/Pages/LandingPage/FollowUsSocial.js
+++ src/Pages/LandingPage/FollowUsSocial.js
@@ -1,20 +1,77 @@
 import React from 'react'
 import followUs from '../../CSS/followUs.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button} from 'antd';
+import 'antd/dist/antd.css';
 import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
 export default function FollowUsSocial() {
+
+      const Twitter = () => (
+        <Button type="" shape="circle">
+        <a href="https://twitter.com/UnderdogDevs" >
+            <FontAwesomeIcon icon={['fab', "twitter"]} />  
+        </a>
+        </Button>)
+
+    const Youtube = () => (
+    <Button type="" shape="circle">
+        <a href="https://youtube.com/channel/UC8rwCavxrhcfZTYPnR_VdSg" >
+            <FontAwesomeIcon icon={['fab', "youtube"]} />
+        </a>
+    </Button>)
+
+    const Slack = () => (
+    <Button type="" shape="circle">
+        <a href="http://underdog-devs.slack.com/">
+            <FontAwesomeIcon icon={['fab', "slack"]} />
+        </a>
+    </Button>)
+
+    const GitHub = () => (
+    <Button type="" shape="circle">
+        <a href="https://github.com/Underdog-Devs">
+            <FontAwesomeIcon icon={['fab', "github"]} />
+        </a>
+    </Button>)
+
+    const Google = () => (
+    <Button type="" shape="circle">
+        <a href="mailto:rickw@underdogdevs.org">
+            <FontAwesomeIcon icon={['fab', "google"]} />
+        </a>
+    </Button>)
+
+
+
   return (
-    <div className="tweetContainer">
-      <TwitterTweetEmbed
-        tweetId={'1340821358184460289'}
-      />
+    <div className="follow-us">
+      {/* header with links */}
+      <div className='follow-us-header'>
+        <h5>Follow Us On <strong>Social Media</strong></h5>
+          <ul className="icons">
+            <Twitter/>
+            <Youtube/>
+            <Slack/>
+            <GitHub/>
+            <Google/>
+          </ul>
+      </div>
 
-      <TwitterTweetEmbed
-        tweetId={'1340821358184460289'}
-      />
+      {/* tweetContainer */}
+      <div className="tweetContainer">
+        <TwitterTweetEmbed
+          tweetId={'1340821358184460289'}
+        />
 
-      <TwitterTweetEmbed
-        tweetId={'1340821358184460289'}
-      />
+        <TwitterTweetEmbed
+          tweetId={'1340821358184460289'}
+        />
+
+        <TwitterTweetEmbed
+          tweetId={'1340821358184460289'}
+        />
+      </div>
     </div>
+    
   )
 }
