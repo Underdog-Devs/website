e18f30fc2a933c6937aa572a76304c7a18b039db
robertjallen <bobbidigi34@gmail.com>
2021-01-25 16:14:56 -0500

    add update styles

--- src/Pages/LandingPage/OurStory.js
+++ src/Pages/LandingPage/OurStory.js
@@ -1,15 +1,17 @@
 import React from 'react'
 // import 'antd/dist/antd.css';
 // import { Card } from 'antd';
+import '../../CSS/OurStory.css'
 import coworkers from '../../Assets/co_workers.png'
 
 // const { Meta } = Card;
 
 export default function OurStory() {
   return (
-    <div>
-       
-    <img alt="example" src={coworkers} />
+    <div className="our-story">
+        <h3 className="our-story-header">Our Story</h3>
+        <p>We are a group of software developers supporting the formerly incarcerated in their transition into the software industry.</p>
+        <img alt="example" src={coworkers} />
     </div>
   )
 }
