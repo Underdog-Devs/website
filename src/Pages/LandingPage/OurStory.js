77b9146a9022734808be539f3a057a268f8f7f74
robertjallen <bobbidigi34@gmail.com>
2021-01-25 15:30:44 -0500

    add refactor

--- src/Pages/LandingPage/OurStory.js
+++ src/Pages/LandingPage/OurStory.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import 'antd/dist/antd.css';
-import { Card } from 'antd';
+// import 'antd/dist/antd.css';
+// import { Card } from 'antd';
 import coworkers from '../../Assets/co_workers.png'
 
 // const { Meta } = Card;
@@ -9,8 +9,7 @@ export default function OurStory() {
   return (
     <div>
        
-    <img alt="example" src={coworkers} />}
-  >
+    <img alt="example" src={coworkers} />
     </div>
   )
 }
