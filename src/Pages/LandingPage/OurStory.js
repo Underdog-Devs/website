94884052a3c03455b740ce98b798176e739e8b6d
robertjallen <bobbidigi34@gmail.com>
2021-01-25 17:12:44 -0500

    add updated styles from mockup

--- src/Pages/LandingPage/OurStory.js
+++ src/Pages/LandingPage/OurStory.js
@@ -5,9 +5,16 @@ import coworkers from '../../Assets/co_workers.png'
 export default function OurStory() {
   return (
     <div className="our-story">
-        <h3 className="our-story-header">Our Story</h3>
-        <p>We are a group of software developers supporting the formerly incarcerated in their transition into the software industry.</p>
-        <img alt="example" src={coworkers} />
+        {/* hero above */}
+        <div className="our-story-above">
+          <h3 className="our-story-header">Our Story</h3>
+          <p>We are a group of software developers supporting the formerly incarcerated in their transition into the software industry.</p>
+        </div>
+
+        {/* hero image */}
+        <div className="our-story-hero">
+          <img className="our-story-hero-image" alt="example" src={coworkers} />
+        </div>
     </div>
   )
 }
