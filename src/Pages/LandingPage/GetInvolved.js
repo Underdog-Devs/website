0059cf5ade13ac05370fc341bd31bdbc537c0f18
robertjallen <bobbidigi34@gmail.com>
2021-01-27 03:40:17 -0500

    add refactor

--- src/Pages/LandingPage/GetInvolved.js
+++ src/Pages/LandingPage/GetInvolved.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from "../../CSS/GetInvolved.module.css"
-import {Row, Col, Button, Input} from 'antd'
+import {Row, Button, Input} from 'antd'
 import {MailOutlined} from '@ant-design/icons'
 
 
