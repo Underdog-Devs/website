c04cfcfb834098fa41428026c814f63cc5fee235
robertjallen <bobbidigi34@gmail.com>
2021-01-26 20:34:21 -0500

    add component

--- src/Pages/LandingPage/landingPage.js
+++ src/Pages/LandingPage/landingPage.js
@@ -3,11 +3,14 @@ import FollowUsSocial from './FollowUsSocial';
 import MetricsComponent from './MetricsComponent';
 import GetInvolved from './GetInvolved';
 import styles from '../../CSS/LandingPage.module.css';
+import WhatIsAMentee from '../LandingPage/WhatIsAMentee'
 
 export default function LandingPage() {
   return (
     <div className={styles.LandingPage}>
       <MetricsComponent />
+
+      <WhatIsAMentee />
       <FollowUsSocial />
       <GetInvolved />
       {/*Landing Page Components Here!*/}
