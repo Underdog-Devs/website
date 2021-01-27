5ff6cced7610f20baaa5ba31084c4726ef3ca602
robertjallen <bobbidigi34@gmail.com>
2021-01-26 20:39:53 -0500

    fix issue

--- src/Pages/LandingPage/landingPage.js
+++ src/Pages/LandingPage/landingPage.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import FollowUsSocial from './FollowUsSocial';
 import MetricsComponent from './MetricsComponent';
-import GetInvolved from './GetInvolved';
-import styles from '../../CSS/LandingPage.module.css';
-import WhatIsAMentee from '../LandingPage/WhatIsAMentee'
+import '../../CSS/LandingPage.css'
+import OurStory from './OurStory';
+import WhatIsAMentee from './WhatIsAMentee';
 
 export default function LandingPage() {
   return (
-    <div className={styles.LandingPage}>
+    <div className='landing-page'>
+      <OurStory/>
       <MetricsComponent />
-
       <WhatIsAMentee />
       <FollowUsSocial />
-      <GetInvolved />
+      
       {/*Landing Page Components Here!*/}
     </div>
   );
