bad140a36f4082511a9bf8905d3f8bc9216155c6
robertjallen <bobbidigi34@gmail.com>
2021-01-28 18:56:05 -0500

    add refactor after fix from css modules dupe file

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import FollowUsSocial from './FollowUsSocial';
 import MetricsComponent from './MetricsComponent';
-import '../../CSS/LandingPage.css'
+import styles from '../../CSS/LandingPage.module.css'
 import OurStory from './OurStory';
 
 export default function LandingPage() {
--- src/Pages/LandingPage/OurStory.js
+++ src/Pages/LandingPage/OurStory.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import '../../CSS/OurStory.css'
+import styles from '../../CSS/OurStory.module.css'
 import coworkers from '../../Assets/co_workers.png'
 
 export default function OurStory() {
   return (
-    <div className="our-story">
+    <div className={styles.ourStory}>
         {/* hero above */}
-        <div className="our-story-above">
-          <h3 className="our-story-header">Our Story</h3>
+        <div className={styles.ourStoryAbove}>
+          <h3 className={styles.ourStoryHeader}>Our Story</h3>
           <p>We are a group of software developers supporting the formerly incarcerated in their transition into the software industry.</p>
         </div>
 
         {/* hero image */}
-        <div className="our-story-hero">
-          <img className="our-story-hero-image" alt="example" src={coworkers} />
+        <div className={styles.ourStoryHero}>
+          <img className={styles.ourStoryHeroImage} alt="example" src={coworkers} />
         </div>
     </div>
   )
