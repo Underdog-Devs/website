064361398494c61c1aaccb01b7325eafe2cc99c1
robertjallen <bobbidigi34@gmail.com>
2021-01-28 17:16:10 -0500

    add update OurStory classnames and css classes

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import FollowUsSocial from './FollowUsSocial';
 import MetricsComponent from './MetricsComponent';
-import '../../CSS/LandingPage.module.css'
+import styles from '../../CSS/LandingPage.module.css'
 import OurStory from './OurStory';
 
 export default function LandingPage() {
   return (
-    <div className='landing-page'>
+    <div className={styles.landingPage}>
       <OurStory/>
       <MetricsComponent />
       <FollowUsSocial />
