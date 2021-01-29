eb7a5ac7f5c854029729bfa9a46c00ed6fe63185
robertjallen <bobbidigi34@gmail.com>
2021-01-28 19:30:01 -0500

    add refactor from after adding ccs mods and dupe

--- src/Pages/LandingPage/WhatIsAMentor.js
+++ src/Pages/LandingPage/WhatIsAMentor.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import meet from '../../Assets/meeting.svg';
-import '../../CSS/WhatIsAMentor.css'
+import styles from '../../CSS/WhatIsAMentor.module.css'
 
 
 export default function WhatIsAMentor() {
   return (
-    <div className="mentor">
+    <div className={styles.mentor}>
         {/* hero above */}
-        <div className="mentor-above">
-          <h3 className="mentor-header">What Is A Mentor?</h3>
+        <div className={styles.mentorAbove}>
+          <h3 className={styles.mentorHeader}>What Is A Mentor?</h3>
           <p>Experienced Professionals, like-minded people, and others with similar interests and goals.</p>
         </div>
 
         {/* hero image */}
-        <div className="mentor-hero">
-          <img className="mentor-hero-image" alt="example" src={meet} />
+        <div className={styles.mentorHero}>
+          <img className={styles.mentorHeroImage} alt="example" src={meet} />
         </div>
     </div>
   )
