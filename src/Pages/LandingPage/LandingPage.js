4ec4f3e293678bfac6bd449a71124e8148748d91
robertjallen <bobbidigi34@gmail.com>
2021-02-08 16:33:15 -0500

    fix ref for LandinngPage.module.css

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import FollowUsSocial from './FollowUsSocial';
 import MetricsComponent from './MetricsComponent';
-import '../../CSS/LandingPage.css'
+import styles from '../../CSS/LandingPage.module.css'
 import OurStory from './OurStory';
 import WhatIsAMentee from './WhatIsAMentee';
 import WhatIsAMentor from './WhatIsAMentor';
 
 export default function LandingPage() {
   return (
-    <div className='landing-page'>
+    <div className={styles.landingPage}>
       <OurStory/>
       <MetricsComponent />
       <WhatIsAMentor/>
