99417f1c4aed5e58c87d18a1035f861abd219917
robertjallen <bobbidigi34@gmail.com>
2021-02-09 00:42:53 -0500

    add fix bad LandingPage.module.css ref

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import FollowUsSocial from './FollowUsSocial';
 import MetricsComponent from './MetricsComponent';
-import '../../CSS/LandingPage.module.css'
+import styles from '../../CSS/LandingPage.module.css'
 import OurStory from './OurStory';
 import WhatIsAMentee from './WhatIsAMentee';
 
 export default function LandingPage() {
   return (
-    <div className='landing-page'>
+    <div className={styles.landingPage}>
       <OurStory/>
       <MetricsComponent />
       <WhatIsAMentee />
       <FollowUsSocial />
-      
-      {/*Landing Page Components Here!*/}
     </div>
   );
 }
