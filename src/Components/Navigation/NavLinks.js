50fb67e54019d6248c0d61b56652ef4f876952b8
William McQuaid <whmcquaid@gmail.com>
2021-09-07 00:53:01 -0400

    Implemented the hmtl from paypal for the donate links

--- src/Components/Navigation/NavLinks.js
+++ src/Components/Navigation/NavLinks.js
@@ -41,14 +41,28 @@ export const NavLinks = ({ isMobile, setOpen }) => {
         </a>
       </li>
       <li>
-        <a
-          href="https://www.paypal.com/paypalme/underdogdevs"
-          className={styles.navLink}
-          rel="noreferrer"
-          target="_blank"
+        <form
+          action="https://www.paypal.com/donate"
+          method="post"
+          target="_top"
         >
-          Donate
-        </a>
+          <input type="hidden" name="hosted_button_id" value="2R2AP79YGLEXJ" />
+          <input
+            type="image"
+            src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
+            border="0"
+            name="submit"
+            title="PayPal - The safer, easier way to pay online!"
+            alt="Donate with PayPal button"
+          />
+          <img
+            alt=""
+            border="0"
+            src="https://www.paypal.com/en_US/i/scr/pixel.gif"
+            width="1"
+            height="1"
+          />
+        </form>
       </li>
       <li>
         <Button
