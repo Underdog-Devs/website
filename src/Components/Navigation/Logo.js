2c9c9eba446ea4e749d4aa2bc90b41212b6dc3f2
Jess Bonanno <jessbonanno@gmail.com>
2021-10-09 11:05:56 -0400

    Revert "Make logo work"

--- src/Components/Navigation/Logo.js
+++ src/Components/Navigation/Logo.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from '../../CSS/Logo.module.css';
-import logo from '../../Assets/UD_Logo.webp';
+import logo from '../../Assets/UDD_logo.svg';
 export default function Logo() {
   return (
     <div className={styles.logo}>
       <img src={logo} alt="Underdog Dev Logo" className={styles.logoImg} />
-      <p className={styles.logoText}> Underdog Devs</p>
+      <p className={styles.logoText}>Underdog Devs</p>
     </div>
   );
 }
